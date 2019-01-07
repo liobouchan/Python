a1e71d5a891d8dea1b3c6a02787c2829347246e8
Lio Bouchan <liogothicvampire@gmail.com>
2019-01-06 22:46:23 -0600

    Terminando con entradas con scripts

---
+++ Scripts/codificacion.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  6 22:44:39 2019
+
+@author: leobo
+"""
+
+print("Este programa tiene código fuente en español")
--- Scripts/escribir_lineas.py
+++ Scripts/escribir_lineas.py
@@ -6,8 +6,13 @@ Created on Sun Jan  6 22:37:35 2019
 """
 
 import sys
-texto = sys.argv[1]
-repeticiones = int(sys.argv[2])
 
-for r in range(repeticiones):
-    print(texto)
+if len(sys.argv) == 3:
+    texto = sys.argv[1]
+    repeticiones = int(sys.argv[2])
+    
+    for r in range(repeticiones):
+        print(texto)
+else:
+    print("Error, introduce los argumentos correctamente")
+    print("Ejemplo: escribir_lineas.py 'Text' #")
