de88708bb26865287b0fd32c381f3b0cd0d091c6
Lio Bouchan <liogothicvampire@gmail.com>
2019-01-06 22:40:16 -0600

    Trabajando scripts con argumentos

---
+++ Scripts/escribir_lineas.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  6 22:37:35 2019
+
+@author: leobo
+"""
+
+import sys
+texto = sys.argv[1]
+repeticiones = int(sys.argv[2])
+
+for r in range(repeticiones):
+    print(texto)
--- Scripts/primer_script.py
+++ Scripts/primer_script.py
@@ -4,5 +4,7 @@ Created on Sun Jan  6 22:28:19 2019
 
 @author: leobo
 """
+import sys
 
 print("Hola, bienvenido a tu primer script")
+print(sys.argv)
