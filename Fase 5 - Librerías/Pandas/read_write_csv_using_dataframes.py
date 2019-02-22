2237e3e02df64fc363a8c0d34c09159240feb5e4
Leonardo <liogothicvampire@gmail.com>
2019-02-22 11:00:01 -0600

    Create a script that read an CSV , do processes with this data in pandas dataFrams and finally exports the data

--- Fase 5 - Librerías/Pandas/read_write_csv_using_dataframes.py
+++ Fase 5 - Librerías/Pandas/read_write_csv_using_dataframes.py
@@ -1,1 +1,23 @@
-impo
+import pandas
+
+data_of_csv = pandas.read_csv("./definiciones.csv" , encoding='UTF-8')
+print("Print the obtained Data from the CSV \n " , data_of_csv)
+
+data_to_dataframe = pandas.DataFrame(data_of_csv)
+print("Print the Obtained Data NOW in a PandasDataFrame \n " , data_to_dataframe)
+
+# I only want the Words that are before the ':' so do the Process
+list_of_words = []
+
+for index, data in data_to_dataframe.iterrows():
+    word = ""
+    if ":" in data["Definicion"]:
+        #Because CSV has Column 'Definicion'
+        word =  data["Definicion"].split(':')[0]
+        list_of_words.append(word)
+print("Print the list of words \n " , list_of_words)
+
+words_dataframe = pandas.DataFrame(list_of_words)
+words_dataframe.columns = ["Definicion"]
+print("Print of the new DataFrame with only the needed words " ,  words_dataframe)
+words_dataframe.to_csv('palabras_de_definiciones.csv' , encoding="UTF-8" , index=False)
